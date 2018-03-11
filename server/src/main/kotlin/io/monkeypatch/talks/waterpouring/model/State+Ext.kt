7d171a524af54a372915de65d2e06c52c9473261
Igor Laborie <ilaborie@gmail.com>
2018-03-11 18:26:37 +0100

    server: soluce 2.2

--- server/src/main/kotlin/io/monkeypatch/talks/waterpouring/model/State+Ext.kt
+++ server/src/main/kotlin/io/monkeypatch/talks/waterpouring/model/State+Ext.kt
@@ -14,14 +14,23 @@ typealias StateWithHistory = Pair<State, List<Move>>
  */
 fun State.availableMoves(): Collection<Move> {
     val glassNotEmptyIndexes: List<Int> =
-        TODO("2.2")
+        mapIndexed { index, glass -> index to glass }
+            .filterNot { (_, glass) -> glass.isEmpty() }
+            .map { it.first }
 
     val glassNotFillIndexes: List<Int> =
-        TODO("2.2")
+        mapIndexed { index, glass -> index to glass }
+            .filterNot { (_, glass) -> glass.isFull() }
+            .map { it.first }
 
-    val empties: List<Move> = TODO("2.2")
-    val fills: List<Move> = TODO("2.2")
-    val pours: List<Move> = TODO("2.2")
+    val empties: List<Move> = glassNotEmptyIndexes.map { Empty(it) }
+    val fills: List<Move> = glassNotFillIndexes.map { Fill(it) }
+    val pours: List<Move> = glassNotEmptyIndexes
+        .flatMap { from ->
+            glassNotFillIndexes
+                .filter { from != it }
+                .map { to -> Pour(from, to) }
+        }
 
     return empties + fills + pours
 }

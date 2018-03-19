8a4542efd909ec6958346bd049298da3bef78946
Igor Laborie <ilaborie@gmail.com>
2018-03-19 23:23:24 +0100

    web: soluce 2.2

--- web/src/main/kotlin/components/final-state.component.kt
+++ web/src/main/kotlin/components/final-state.component.kt
@@ -1,11 +1,15 @@
 package components
 
 import io.monkeypatch.talks.waterpouring.model.State
+import kotlinx.html.InputType.number
 import react.RBuilder
 import react.RComponent
 import react.RProps
 import react.RState
 import react.ReactElement
+import react.dom.div
+import react.dom.input
+import react.dom.span
 
 
 /**
@@ -29,8 +33,21 @@ interface FinalStateProps : RProps {
 
 class FinalStateComponent : RComponent<FinalStateProps, RState>() {
     override fun RBuilder.render() {
-        TODO("2.2: template")
-        TODO("3.3: changer la valeur courante dans l'état final")
+        props.state.mapIndexed { index, glass ->
+            div(classes = "glass-container") {
+                glass(glass)
+                input(type = number, classes = "current") {
+                    attrs {
+                        min = "0"
+                        value = "${glass.current}"
+                        max = "${glass.capacity}"
+                    }
+                }
+                span { +"/" }
+                span(classes = "capacity") { +"${glass.capacity}" }
+            }
+        }
+//        TODO("3.3: changer la valeur courante dans l'état final")
     }
 }
 

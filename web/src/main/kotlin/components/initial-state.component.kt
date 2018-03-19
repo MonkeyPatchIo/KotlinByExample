20996a20cfe76d363bb1e66d79e68e19c69bd534
Igor Laborie <ilaborie@gmail.com>
2018-03-19 23:25:16 +0100

    web: soluce 2.3

--- web/src/main/kotlin/components/initial-state.component.kt
+++ web/src/main/kotlin/components/initial-state.component.kt
@@ -1,11 +1,15 @@
 package components
 
 import io.monkeypatch.talks.waterpouring.model.State
+import kotlinx.html.InputType
 import react.RBuilder
 import react.RComponent
 import react.RProps
 import react.RState
 import react.ReactElement
+import react.dom.div
+import react.dom.input
+import react.dom.span
 
 
 /**
@@ -32,9 +36,29 @@ interface InitialStateProps : RProps {
 
 class InitialStateComponent : RComponent<InitialStateProps, RState>() {
     override fun RBuilder.render() {
-        TODO("2.3: template")
-        TODO("3.4: changer la valeur courante dans l'état initial")
-        TODO("3.5: changer la capacité dans l'état initial")
+
+        props.state.mapIndexed { index, glass ->
+            div(classes = "glass-container") {
+                glass(glass)
+                input(type = InputType.number, classes = "current") {
+                    attrs {
+                        min = "0"
+                        value = "${glass.current}"
+                        max = "${glass.capacity}"
+                    }
+                }
+                span { +"/" }
+                input(type = InputType.number, classes = "capacity") {
+                    attrs {
+                        min = "${props.minCapacity}"
+                        value = "${glass.capacity}"
+                        max = "${props.maxCapacity}"
+                    }
+                }
+            }
+        }
+//        TODO("3.4: changer la valeur courante dans l'état initial")
+//        TODO("3.5: changer la capacité dans l'état initial")
     }
 }
 

01d9fc0a5984cd00973799fa2df0555c50c577f8
Igor Laborie <ilaborie@gmail.com>
2018-03-20 00:09:06 +0100

    web: soluce 4.6

--- web/src/main/kotlin/components/solution-state.component.kt
+++ web/src/main/kotlin/components/solution-state.component.kt
@@ -2,11 +2,15 @@ package components
 
 import io.monkeypatch.talks.waterpouring.model.Move
 import io.monkeypatch.talks.waterpouring.model.State
+import model.toDisplayString
 import react.RBuilder
 import react.RComponent
 import react.RProps
 import react.RState
 import react.ReactElement
+import react.dom.div
+import react.dom.li
+import react.dom.span
 
 
 /**
@@ -33,7 +37,21 @@ interface SolutionStateProps : RProps {
 class SolutionStateComponent : RComponent<SolutionStateProps, RState>() {
 
     override fun RBuilder.render() {
-        TODO("4.6")
+        props.solutionList.map { (move, state) ->
+            li {
+                div(classes = "move") {
+                    +(move?.toDisplayString() ?: "")
+                }
+                div(classes = "state") {
+                    state.map { glass ->
+                        div(classes = "glass-container") {
+                            glass(glass)
+                            span { +"${glass.current} / ${glass.capacity}" }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 

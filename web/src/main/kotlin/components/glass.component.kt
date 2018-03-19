69af924a971df19503812b9fb0122a05844c25ed
Igor Laborie <ilaborie@gmail.com>
2018-03-19 23:14:01 +0100

    web: soluce 2.1

--- web/src/main/kotlin/components/glass.component.kt
+++ web/src/main/kotlin/components/glass.component.kt
@@ -1,11 +1,15 @@
 package components
 
 import io.monkeypatch.talks.waterpouring.model.Glass
+import kotlinext.js.js
+import model.fillPercent
 import react.RBuilder
 import react.RComponent
 import react.RProps
 import react.RState
 import react.ReactElement
+import react.dom.div
+import react.dom.jsStyle
 
 /**
  * Create React component for a [Glass]:
@@ -24,7 +28,12 @@ interface RGlassProps : RProps {
 
 class GlassComponent : RComponent<RGlassProps, RState>() {
     override fun RBuilder.render() {
-        TODO("2.1")
+        div(classes = "glass") {
+            attrs.jsStyle = js {
+                height = "${props.glass.capacity}em"
+                backgroundImage = "linear-gradient(to top, chocolate ${props.glass.fillPercent()}%, transparent 0px)"
+            }
+        }
     }
 }
 
